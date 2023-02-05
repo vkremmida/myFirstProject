6ad2325aa23d2d9b68f08dff0340737cb4f641ca
vivi <vivi_232002@yahoo.gr>
2023-02-05 19:34:41 +0200

    Final Assignment!!!!

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import pandas as pd
+import numpy as np
+df = pd.read_csv("finance_liquor_sales_1.csv")
+df["Percentage of sales per store"] = ((df["sale_dollars"] / df["sale_dollars"].sum()) * 100)
+cn = df.groupby(['zip_code','Percentage of sales per store'])
+print(cn.aggregate(np.sum))
+df.to_csv('finance_liquor_sales_2.csv')
+
+
+
