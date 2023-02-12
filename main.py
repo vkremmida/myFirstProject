100569937edb2da9b4403cf5ad1dd08a9678d4a3
vivi <vivi_232002@yahoo.gr>
2023-02-13 01:37:01 +0200

    Final Assignment!!!!

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 df = pd.read_csv("finance_liquor_sales_1.csv")
-df["Percentage of sales per store"] = ((df["sale_dollars"] / df["sale_dollars"].sum()) * 100)
-cn = df.groupby(['zip_code','Percentage of sales per store'])
-print(cn.aggregate(np.sum))
+df.groupby(['zip_code','store_number']).sum()
+#df["Percentage of sales per store"] = df.groupby(['store_number'])["sale_dollars"] / df["sale_dollars"].sum() * 100)
 df.to_csv('finance_liquor_sales_2.csv')
+x = np.array(df['zip_code'])
 
+y = np.array(df['bottles_sold'])
 
+plt.scatter(x, y)
+plt.title("Bottles Sold")
+plt.xlabel("Zip Code")
+plt.ylabel("Bottles Sold")
 
+plt.show()
